bff3c6921e17c5c8c606ab01446dbfab8cc0570c
Artem <labauriatm@gmail.com>
2024-06-06 21:47:34 +0300

    Added new file eslint and fixed some bugs.

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlineSync from 'readline-sync'; 
+import readlineSync from 'readline-sync';
 
 const name = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hi, ' + userName + '!');  
-}
+  console.log(`Hi, ${userName}!`);
+};
 
-export default name; 
+export default name;
